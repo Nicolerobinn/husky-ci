139f46aefa3c1a3b8273518dd2f67e070ffa0283
Nick <cweb130@163.com>
2021-12-06 17:17:37 +0800

    feat: main
    增加入口

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 'use strict'
+module.exports = () => {
 
-if (process.platform !== 'Linux') {
-  const fs = require('fs');
-  fs.access('.husky', fs.constants.F_OK, (err) => {
-    if (err) {
-      let husky
-      try {
-        husky = require('husky');
-      } catch (error) {
-        console.error('这不是一个错误，只是没有安装husky模块')
-        return
+  if (process.platform !== 'Linux') {
+    const fs = require('fs');
+    fs.access('.husky', fs.constants.F_OK, (err) => {
+      if (err) {
+        let husky
+        try {
+          husky = require('husky');
+        } catch (error) {
+          console.error('这不是一个错误，只是没有安装husky模块')
+          return
+        }
+        husky.install();
+        husky.add('.husky/commit-msg', 'yarn commitlint --edit "$1"');
+        husky.add('.husky/pre-commit', 'yarn lint-staged');
       }
-      husky.install();
-      husky.add('.husky/commit-msg', 'yarn commitlint --edit "$1"');
-      husky.add('.husky/pre-commit', 'yarn lint-staged');
-    }
-  });
-} else {
-  console.log('当前不在开发环境')
-}
+    });
+  } else {
+    console.log('当前不在开发环境')
+  }
 
+}
