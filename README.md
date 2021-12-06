dbae9be3c64f76970cbde0017781be67e1067216
Nick <cweb130@163.com>
2021-12-06 18:09:28 +0800

    feat: 修改调用模式

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # husky-ci
-主要用于在容器中只有pacjage.json的情况
+
+需要配合依赖使用
+"husky": "^7.0.4",
+"lint-staged": "^12.1.2",
+"@commitlint/cli": "^15.0.0",
+"@commitlint/config-conventional": "^15.0.0",
+
+主要用于在容器中只有 pacjage.json 的情况
+直接调用脚本就好
+
+```javascript
+  node node_modules/husky-ci
+```
--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 'use strict'
-module.exports = () => {
-
-  if (process.platform !== 'Linux') {
-    const fs = require('fs');
-    fs.access('.husky', fs.constants.F_OK, (err) => {
-      if (err) {
-        let husky
-        try {
-          husky = require('husky');
-        } catch (error) {
-          console.error('这不是一个错误，只是没有安装husky模块')
-          return
-        }
-        husky.install();
-        husky.add('.husky/commit-msg', 'yarn commitlint --edit "$1"');
-        husky.add('.husky/pre-commit', 'yarn lint-staged');
+if (process.platform !== 'Linux') {
+  const fs = require('fs');
+  fs.access('.husky', fs.constants.F_OK, (err) => {
+    if (err) {
+      let husky
+      try {
+        husky = require('husky');
+      } catch (error) {
+        console.log("%c没有安装husky模块，不会执行创建命令 %c", "color:red", "", "color:orange;font-weight:bold")
+        return
       }
-    });
-  } else {
-    console.log('当前不在开发环境')
-  }
-
+      husky.install();
+      husky.add('.husky/commit-msg', 'yarn commitlint --edit "$1"');
+      husky.add('.husky/pre-commit', 'yarn lint-staged');
+    } else {
+      console.log("%c这不是一个错误,当前已经存在husky文件，不会重复生成 %c", "color:red", "", "color:orange;font-weight:bold")
+    }
+  });
+} else {
+  console.log('当前不在开发环境，当前环境为' + process.platform)
 }
