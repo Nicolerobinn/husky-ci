f0fdeb704fbeff41f8729a21cf6f3ddd26bbb5d7
Nick <cweb130@163.com>
2021-12-06 18:24:41 +0800

    feat: 更改校验

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
-if (process.platform !== 'Linux') {
+if (process.platform !== 'linux') {
   const fs = require('fs');
   fs.access('.husky', fs.constants.F_OK, (err) => {
     if (err) {
@@ -7,14 +7,14 @@ if (process.platform !== 'Linux') {
       try {
         husky = require('husky');
       } catch (error) {
-        console.log("%c没有安装husky模块，不会执行创建命令 %c", "color:red", "", "color:orange;font-weight:bold")
+        console.log("没有安装husky模块，不会执行创建命令")
         return
       }
       husky.install();
       husky.add('.husky/commit-msg', 'yarn commitlint --edit "$1"');
       husky.add('.husky/pre-commit', 'yarn lint-staged');
     } else {
-      console.log("%c这不是一个错误,当前已经存在husky文件，不会重复生成 %c", "color:red", "", "color:orange;font-weight:bold")
+      console.log("这不是一个错误,当前已经存在husky文件，不会重复生成 ")
     }
   });
 } else {
